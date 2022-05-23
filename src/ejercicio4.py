17796efb1bcafeefbdb7c04506f81a1d290c4540
Bianca Fillol <biancafillol24@gmail.com>
2022-05-23 19:42:53 -0300

    Se ha creado ejercicio4.py

---
+++ src/ejercicio4.py
@@ -0,0 +1,27 @@
+from plantilla import principal
+################
+# Bianca Fillol - @bianfillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+#PRECONDICIÓN: Ingresar dos números enteros.
+#POSTCONDICIÓN: Obtener la suma lenta de los dos números enteros.
+def suma_lenta(numero, otro_numero):
+    """Esta función suma el primer número con el segundo número,
+    (el segundo es separado por dígitos en 1 en 1).
+    """
+    print(numero,end="")
+    resultado=numero+otro_numero
+    if otro_numero>0:
+        for otro in range(otro_numero):
+            otro=1
+            print(f" + {otro}",end="")
+        print(f" = {resultado}")
+    else:
+        otro_numero=otro_numero*-1
+        for otro in range(otro_numero):
+            otro=1
+            print(f" - {otro}",end="")
+        print(f" = {resultado}")
+suma_lenta(numero=int(input("Ingrese el primer número: ")),
+otro_numero=int(input("Ingrese el segundo número: ")))
+principal()
