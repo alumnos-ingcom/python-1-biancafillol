302a49a7c4e370286cb8d8e76bed59953eefb825
Bianca Fillol <biancafillol24@gmail.com>
2022-05-23 20:04:42 -0300

    Se ha modificado el método de suma lenta de ejercicio4.py

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -10,18 +10,19 @@ def suma_lenta(numero, otro_numero):
     (el segundo es separado por dígitos en 1 en 1).
     """
     print(numero,end="")
-    resultado=numero+otro_numero
     if otro_numero>0:
         for otro in range(otro_numero):
             otro=1
             print(f" + {otro}",end="")
-        print(f" = {resultado}")
+            numero=numero+1
+        print(f" = {numero}")
     else:
         otro_numero=otro_numero*-1
         for otro in range(otro_numero):
             otro=1
             print(f" - {otro}",end="")
-        print(f" = {resultado}")
+            numero=numero-1
+        print(f" = {numero}")
 suma_lenta(numero=int(input("Ingrese el primer número: ")),
 otro_numero=int(input("Ingrese el segundo número: ")))
 principal()
