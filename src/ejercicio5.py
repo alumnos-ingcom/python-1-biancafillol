4a948c40736641c9e55e4d0c97ce214c162dd7a5
Bianca Fillol <biancafillol24@gmail.com>
2022-05-23 20:59:48 -0300

    Se ha creado el ejercicio5.py

---
+++ src/ejercicio5.py
@@ -0,0 +1,24 @@
+from plantilla import principal
+################
+# Bianca Fillol - @bianfillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+#PRECONDICIÓN: Ingresar dos números enteros.
+#POSTCONDICIÓN: Obtener el cociente y el resto mediante resta sucesiva.
+def division_lenta(dividendo, divisor):
+    """Esta función hace que el dividendo sea restado sucevisamente
+    por el divisor hasta que el resultado de menor o igual al dividendo.
+    """
+    cociente=0
+    print(dividendo,end="")
+    while dividendo>=divisor:
+        print(f" - {divisor}",end="")
+        dividendo=dividendo-divisor
+        cociente=cociente+1
+    resto=dividendo
+    print(f" = {resto}")
+    print(f"El cociente es: {cociente}")
+    print(f"El resto es: {resto}")
+division_lenta(dividendo=int(input("Ingrese el dividendo: ")),
+divisor=int(input("Ingrese el divisor: ")))
+principal()
