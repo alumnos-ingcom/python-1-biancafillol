8144b7edae91c57a19bce5c40d326ea5fba67b16
Bianca Fillol <biancafillol24@gmail.com>
2022-05-23 01:13:14 -0300

    Se ha modificado ejercicio1.py de acuerdo a las correciones del bot.

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -3,11 +3,7 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 #PRECONDICIÓN: Ingresar un número decimal expresado en grados centigrados,
-#e ingresar un número decimal expresado en grainput("Ingrese los grados fahrenheit: "))))
-def principal():
-    pass
-if __name__ == "__main__":
-    principal()dos fahrenheit.
+#e ingresar un número decimal expresado en grados fahrenheit.
 #POSTCONDICIÓN: Obtener un número decimal expresado en grados fahrenheit,
 #y obtener un número decimal expresado en grados centigrados.
 def convertir_a_fahrenheit(centigrados):
@@ -15,18 +11,17 @@ def convertir_a_fahrenheit(centigrados):
     convertir_a_fahrenheit: Esta función se encarga de convertir
     los grados centigrados a fahrenheit.
     """
-    convertir_a_fahrenheit=((9*centigrados)/5)+32
-    return "Convertidos a grados fahrenheit son: " + str(convertir_a_fahrenheit) + "° grados."
+    return f"Convertidos a grados fahrenheit son: {((9*centigrados)/5)+32}° grados."
 def convertir_a_centigrados(fahrenheit):
     """
     convertir_a_centigrados: Esta función se encarga de convertir
     los grados fahrenheit a grados centigrados.
     """
-    convertir_a_centigrados=(5*(fahrenheit-32))/9
-    return "Convertidos a grados centigrados son: " + str(convertir_a_centigrados) + "° grados."
+    return f"Convertidos a grados centigrados son: {(5*(fahrenheit-32))/9}° grados."
 print(convertir_a_fahrenheit(centigrados=float(input("Ingrese los grados centigrados: "))))
 print(convertir_a_centigrados(fahrenheit=float(input("Ingrese los grados fahrenheit: "))))
 def principal():
     pass
 if __name__ == "__main__":
     principal()
+
