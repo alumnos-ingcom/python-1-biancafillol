1ccefb612edb265c3d64c59cbba9a2db779e863d
Bianca Fillol <biancafillol24@gmail.com>
2022-05-24 19:12:18 -0300

    Se agregó una especificación al return de cada función en ejercicio1.py

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -9,11 +9,11 @@
 def convertir_a_fahrenheit(centigrados):
     """Esta función convierte los grados centigrados a fahrenheit."""
     resultado_f=((9*centigrados)/5)+32
-    return f"{centigrados}° convertidos a grados fahrenheit son: {resultado_f}° grados."
+    return f"{centigrados}° centigrados convertidos a grados fahrenheit son: {resultado_f}° grados."
 def convertir_a_centigrados(fahrenheit):
     """Esta función convierte los grados fahrenheit a centigrados."""
     resultado_c=(5*(fahrenheit-32))/9
-    return f"{fahrenheit}° convertidos a grados centigrados son: {resultado_c}° grados."
+    return f"{fahrenheit}° fahrenheit convertidos a grados centigrados son: {resultado_c}° grados."
 def principal():
     """Esta función se encarga de la parte 'interactiva' del programa.
     """
