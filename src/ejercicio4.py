d8c0933ffa8bf092e6b4d095feddef3ea9f41380
Bianca Fillol <biancafillol24@gmail.com>
2022-05-24 18:10:33 -0300

    Se ha agregado la entrada a principal() en ejercicio4.py

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -1,31 +1,22 @@
-################
-# Bianca Fillol - @bianfillol
-# UNRN Andina - Introducción a la Ingenieria en Computación
-################
-#PRECONDICIÓN: Ingresar dos números enteros.
-#POSTCONDICIÓN: Obtener la suma lenta de los dos números enteros.
 def suma_lenta(numero, otro_numero):
     """Esta función suma el primer número con el segundo número,
     (el segundo es separado por dígitos en 1 en 1).
     """
+    contador=0
     print(numero,end="")
-    if otro_numero>0:
-        for otro in range(otro_numero):
-            otro=1
-            print(f" + {otro}",end="")
+    while contador<abs(otro_numero):
+        if otro_numero>0:
+            print(" + 1",end="")
             numero=numero+1
-        print(f" = {numero}")
-    else:
-        otro_numero=otro_numero*-1
-        for otro in range(otro_numero):
-            otro=1
-            print(f" - {otro}",end="")
+        else:
+            print(" - 1",end="")
             numero=numero-1
-        print(f" = {numero}")
-suma_lenta(numero=int(input("Ingrese el primer número: ")),
-otro_numero=int(input("Ingrese el segundo número: ")))
+        contador=contador+1
+    print(f" = {numero}")
 def principal():
-    pass
-
+    """Esta función se encarga de la parte 'interactiva' del programa.
+    """
+    suma_lenta(numero=int(input("Ingrese el primer número: ")),
+    otro_numero=int(input("Ingrese el segundo número: ")))
 if __name__ == "__main__":
     principal()
