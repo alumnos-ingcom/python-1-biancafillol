6665b53ab6588e5a02b84b2dbc05818e72262d25
Bianca Fillol <biancafillol24@gmail.com>
2022-05-25 20:37:13 -0300

    Se movió la salida a principal() en ejercicio1.py

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -9,17 +9,19 @@
 def convertir_a_fahrenheit(centigrados):
     """Esta función convierte los grados centigrados a fahrenheit."""
     resultado_f=((9*centigrados)/5)+32
-    return f"{centigrados}° centigrados convertidos a grados fahrenheit son: {resultado_f}° grados."
+    return resultado_f
 def convertir_a_centigrados(fahrenheit):
     """Esta función convierte los grados fahrenheit a centigrados."""
     resultado_c=(5*(fahrenheit-32))/9
-    return f"{fahrenheit}° fahrenheit convertidos a grados centigrados son: {resultado_c}° grados."
+    return resultado_c
 def principal():
     """Esta función se encarga de la parte 'interactiva' del programa.
     """
     centigrados=float(input("Ingrese los grados centigrados: "))
     fahrenheit=float(input("Ingrese los grados fahrenheit: "))
-    print(convertir_a_fahrenheit(centigrados))
-    print(convertir_a_centigrados(fahrenheit))
+    respuesta_f=convertir_a_fahrenheit(centigrados)
+    respuesta_c=convertir_a_centigrados(fahrenheit)
+    print(f"{centigrados}° convertidos a fahrenheit son {respuesta_f}° grados.")
+    print(f"{fahrenheit}° convertidos a fahrenheit son {respuesta_c}° grados.")
 if __name__ == "__main__":
     principal()
