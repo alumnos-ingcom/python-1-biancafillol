7c7fc48552a8c5989674833e3d73a29cb8a72a7e
Bianca Fillol <biancafillol24@gmail.com>
2022-05-26 12:36:50 -0300

    Se agrego un fstring a salida de principal() en ejercicio7.py

--- src/ejercicio7.py
+++ src/ejercicio7.py
@@ -10,22 +10,25 @@ def sexadecimal_a_decimal(horas, minutos, segundos):
     """Esta función convierte las horas, minutos y segundos a segundos.
     """
     minutos=minutos+(horas*60)
-    numero=segundos+(minutos*60)
-    return numero
+    resp=segundos+(minutos*60)
+    return resp
 def decimal_a_sexadecimal(numero):
     """Esta función convierte los segundos a horas, minutos y segundos.
     """
     horas_n= (numero //60)//60
     minutos_n=(numero//60)%60
     segundos_n=numero%60
-    return f"{numero} segundos es equivalente a: {[horas_n, minutos_n, segundos_n]}."
+    respuest= [horas_n, minutos_n, segundos_n]
+    return respuest
 def principal():
     """Esta función se encarga de la parte 'interactiva' del programa.
     """
     horas=int(input("Ingrese las horas: "))
     minutos=int(input("Ingrese los minutos: "))
     segundos=int(input("Ingrese los segundos: "))
-    print(f"El total es de: {sexadecimal_a_decimal(horas, minutos, segundos)} segundos.")
-    print(decimal_a_sexadecimal(numero=sexadecimal_a_decimal(horas, minutos, segundos)))
+    numero=sexadecimal_a_decimal(horas, minutos, segundos)
+    resultado=decimal_a_sexadecimal(numero)
+    print(f"En total son {numero} segundos.")
+    print(f"{numero} segundos es equivalente a: {resultado}.")
 if __name__ == "__main__":
     principal()
