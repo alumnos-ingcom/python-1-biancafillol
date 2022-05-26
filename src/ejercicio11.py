9095596d6da903dfae6f8e537feac44015c7005f
Bianca Fillol <biancafillol24@gmail.com>
2022-05-26 16:24:38 -0300

    Se ha agregado ejercicio11.py

---
+++ src/ejercicio11.py
@@ -0,0 +1,23 @@
+################
+# Bianca Fillol - @bianfillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+#PRECONDICIÓN: Ingresar dos números enteros positivos.
+#POSTCONDICIÓN: Obtener un True o False.
+def es_multiplo(numero, multiplo):
+    """Esta función determina si el segundo número es múltiplo del primero.
+    """
+    mul=0
+    while mul<multiplo:
+        mul=mul+numero
+    resultado= mul==multiplo
+    return resultado
+def principal():
+    """Esta función se encarga de la parte 'interactiva' del programa.
+    """
+    numero=int(input("Ingrese un número entero positivo: "))
+    multiplo=int(input("Ingrese un número entero positivo: "))
+    respuesta=es_multiplo(numero, multiplo)
+    print(respuesta)
+if __name__=="__main__":
+    principal()
