d1e0f32ebf48d4371b1d3c57927b1dc25ad5c97e
Bianca Fillol <biancafillol24@gmail.com>
2022-05-30 11:50:53 -0300

    Se ha crado test_ejercicio1.py

---
+++ tests/test_ejercicio1.py
@@ -0,0 +1,16 @@
+import pytest
+from src.ejercicio1 import convertir_a_fahrenheit, convertir_a_centigrados
+def test_convertir_a_fahrenheit():
+    """Esta función evalúa si convertir_a_fahrenheit funciona correctamente.
+    """
+    numero=23.3
+    resultado=convertir_a_fahrenheit(numero)
+    assert isinstance(resultado, float), "El resultado debe ser un número decimal."
+    assert resultado==73.94, "No se obtiene el resultado esperado." 
+def test_convertir_a_centigrados():
+    """Esta función evalúa si convertir_a_centigrados funciona correctamente.
+    """
+    numero=0.0
+    resultado=convertir_a_fahrenheit(numero)
+    assert isinstance(resultado, float), "El resultado debe ser un número decimal."
+    assert resultado==32.0, "No se obtiene el resultado esperado."
