81cbd1b0c4fd574ee1891d3241142c852ffde352
Bianca Fillol <biancafillol24@gmail.com>
2022-05-31 19:32:07 -0300

    Se renombraron variables y se modificó el valor de fahrenheit.

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -5,15 +5,15 @@ from src.ejercicio1 import convertir_a_fahrenheit, convertir_a_centigrados
 def test_convertir_a_fahrenheit():
     """Esta función evalúa si convertir_a_fahrenheit funciona correctamente.
     """
-    numero=23.3
-    resultado=convertir_a_fahrenheit(numero)
+    centigrados=23.3
+    resultado=convertir_a_fahrenheit(centigrados)
     assert isinstance(resultado, float), "El resultado debe ser un número decimal."
     assert resultado==73.94, "No se obtiene el resultado esperado."
     return resultado
 def test_convertir_a_centigrados():
     """Esta función evalúa si convertir_a_centigrados funciona correctamente.
     """
-    numero=0.0
+    fahrenheit=77.0
     resultado=convertir_a_centigrados(numero)
     assert isinstance(resultado, float), "El resultado debe ser un número decimal."
-    assert resultado==32.0, "No se obtiene el resultado esperado."
+    assert resultado==25.0, "No se obtiene el resultado esperado."
