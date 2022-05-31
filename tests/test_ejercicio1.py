d065a8a2c43cbdec93e75dfa76d44f94d8079202
Bianca Fillol <biancafillol24@gmail.com>
2022-05-31 19:27:44 -0300

    Se modificó una llamada a función y se agregó un dosctring.

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -1,3 +1,5 @@
+"""Ejercicio 1
+"""
 import pytest
 from src.ejercicio1 import convertir_a_fahrenheit, convertir_a_centigrados
 def test_convertir_a_fahrenheit():
@@ -7,10 +9,11 @@ def test_convertir_a_fahrenheit():
     resultado=convertir_a_fahrenheit(numero)
     assert isinstance(resultado, float), "El resultado debe ser un número decimal."
     assert resultado==73.94, "No se obtiene el resultado esperado."
+    return resultado
 def test_convertir_a_centigrados():
     """Esta función evalúa si convertir_a_centigrados funciona correctamente.
     """
     numero=0.0
-    resultado=convertir_a_fahrenheit(numero)
+    resultado=convertir_a_centigrados(numero)
     assert isinstance(resultado, float), "El resultado debe ser un número decimal."
     assert resultado==32.0, "No se obtiene el resultado esperado."
