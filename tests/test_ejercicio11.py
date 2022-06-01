e16966821f1a08c6e72b2a958369cb000ddfe8a0
Bianca Fillol <biancafillol24@gmail.com>
2022-06-01 02:43:41 -0300

    Se ha creado test_ejercicio11.py

---
+++ tests/test_ejercicio11.py
@@ -0,0 +1,53 @@
+"""Test del Ejercicio 11
+"""
+################
+# Bianca Fillol - @bianfillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+from src.ejercicio10 import es_multiplo
+def test_es_multiplo_mayor_primero_positivos_false():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=8
+    multiplo=2
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano."
+    assert resultado is False, "No se obtiene el resultado esperado."
+def test_es_multiplo_mayor_segundo_positivos_true():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=3
+    multiplo=9
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano."
+    assert resultado is True, "No se obtiene el resultado esperado."
+def test_es_multiplo_mayor_segundo_positivos_false():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=2
+    multiplo=7
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano."
+    assert resultado is False, "No se obtiene el resultado esperado."
+def test_es_multiplo_iguales():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=2
+    multiplo=2
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, bool), "El resultado debe ser un valor booleano."
+    assert resultado is True, "No se obtiene el resultado esperado."
+def test_es_multiplo_ceros():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=0
+    multiplo=0
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, str), "El resultado debe ser un string."
+def test_es_multiplo_negativos():
+    """Esta función evalúa si es_multiplo funciona correctamente.
+    """
+    numero=-4
+    multiplo=-8
+    resultado=es_multiplo(numero, multiplo)
+    assert isinstance(resultado, str), "El resultado debe ser un valor string."
